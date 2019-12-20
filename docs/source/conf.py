eb5d74028398921ed3677b8d51a4a0749b5a8be5
Wilbert Berendsen <wbsoft@xs4all.nl>
2019-12-20 21:02:34 +0100

    some doc fiddling; viewcode does not work...

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,10 +32,12 @@ import livelex.pkginfo
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.coverage',
-    'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+#    'sphinx.ext.doctest',
+#    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -87,6 +89,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+#html_theme = 'alabaster'
 html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
