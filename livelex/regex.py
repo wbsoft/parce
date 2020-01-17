7c6b5b4e66a9aa73d202676aae3444eb0c6fe544
Wilbert Berendsen <wbsoft@xs4all.nl>
2020-01-17 09:22:32 +0100

    add regex.to_string()
    this method checks whether a regex is in fact a simple string
    without special characters, and returns the converted string
    in that case.
    
    this can be used to optimize the case where a lexicon has only one
    pattern rule, and no dynamic actions etc, and str.find can be used
    for lexing.

--- livelex/regex.py
+++ livelex/regex.py
@@ -303,3 +303,50 @@ def common_suffix(words):
         words = [word[:i] for word in words]
     return words, suffix
 
+
+def to_string(expr):
+    """If the regular expression can be converted to a simple string, return it.
+
+    Otherwise, None is returned. The returned string can be used with
+    "".find(), which would be faster than using re.search().
+
+    """
+    try:
+        re.compile(expr)    # this should not fail
+    except re.error:
+        return
+    if set(re.sub(r'\\.', '', expr)) & set("^$|.()[]{}+*?"):
+        return  # there are unescaped special characters like (, [, ? etc.
+    # handle all escapes, there may be fails, in that case we can't use the expr as string
+    pat = (r'\\(?:'
+        r'x([0-9a-fA-F]{2})'    # 1 hex
+        r'|([abfnrtv\\])'       # 2 normal escaped character like \n
+        r'|(\d{1,3})'           # 3 octal , or fail if back ref
+        r'|u([0-9a-fA-F]{4})'   # 4 \uxxxx
+        r'|U([0-9a-fA-F]{8})'   # 5 \Uxxxxxxxx
+        r'|([\^\$\|\.\(\)\[\]\{\}\+\*\?])'  # special re char that was escaped
+        r'|)')
+    def replace_escapes(m):
+        if m.group(1):
+            return chr(int(m.group(1), 16))
+        elif m.group(2):
+            return chr({'a':7, 'b':8, 'f':12, 'n':10, 'r':13, 't':9, 'v':11, '\\':92}[m.group(2)])
+        elif m.group(3):
+            if 0 < int(m.group(3)) <= 99 and not m.group(3).startswith("0"):
+                raise ValueError
+            return chr(int(m.group(3), 8))  # can also raise ValueError
+        elif m.group(4):
+            return chr(int(m.group(4), 16))
+        elif m.group(5):
+            return chr(int(m.group(5), 16))
+        elif m.group(6):
+            return m.group(6)
+        raise ValueError
+    try:
+        s = re.sub(pat, replace_escapes, expr)
+    except ValueError:
+        return
+    assert re.fullmatch(expr, s)
+    return s
+
+
