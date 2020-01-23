3c9dbb505379b0a50157096899d0e6586fcf46cc
Wilbert Berendsen <wbsoft@xs4all.nl>
2020-01-23 01:10:00 +0100

    css wip

--- parce/lang/css.py
+++ parce/lang/css.py
@@ -20,38 +20,47 @@
 
 import re
 
-
 from parce import *
 
-RE_CSS_ESCAPE = r"\\(?:[0-9A-Fa-f]{1,6} ?|\n|.)"
+
+RE_CSS_ESCAPE = r"\\(?:[0-9A-Fa-f]{1,6} ?|.)"
 RE_CSS_NUMBER = (
-    r"[+-]?"            # sign
+    r"[+-]?"             # sign
     r"(?:\d*\.)?\d+"     # mantisse
-    r"([Ee][+-]\d+)?")  # exponent
+    r"([Ee][+-]\d+)?")   # exponent
+RE_CSS_IDENTIFIER = (
+    r"(?:-?(?:[^\W\d]+|" + RE_CSS_ESCAPE + r")|--)"
+    r"(?:[\w-]+|" + RE_CSS_ESCAPE + r")*")
+RE_CSS_AT_KEYWORD = r"@" + RE_CSS_IDENTIFIER
 
 
 class Css(Language):
-    
+    @lexicon
+    def root(cls):
+        yield from cls.common()
+
     @classmethod
     def common(cls):
         yield r'"', String, cls.dqstring
         yield r"'", String, cls.sqstring
         yield r"/\*", Comment, cls.comment
-    
+        yield RE_CSS_NUMBER, Number
+
     @lexicon
     def dqstring(cls):
         yield r'"', String, -1
         yield from cls.string()
-    
+
     @lexicon
     def sqstring(cls):
         yield r"'", String, -1
         yield from cls.string()
-    
+
     @classmethod
     def string(cls):
         yield default_action, String
         yield RE_CSS_ESCAPE, String.Escape
+        yield r"\\\n", String.Escape
         yield r"\n", Error, -1
 
     @lexicon
