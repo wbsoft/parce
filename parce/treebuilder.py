45e08a24cb177df17d764598343778f9186d1dfd
Wilbert Berendsen <wbsoft@xs4all.nl>
2020-01-23 08:02:58 +0100

    remove debug print

--- parce/treebuilder.py
+++ parce/treebuilder.py
@@ -202,7 +202,6 @@ class TreeBuilder:
             done = False
             while not done:
                 for pos, tokens, target in self.parse_context(context, text, pos):
-                    print("!!", tokens, target)
                     if tokens:
                         if head:
                             # move start if the tokens before start didn't change
