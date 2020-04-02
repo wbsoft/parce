6d343f1057e089c052df61605036efd7620c2a6e
Wilbert Berendsen <wbsoft@xs4all.nl>
2020-04-02 17:02:29 +0200

    add test leaf context, which proves this is a lost cause ;)

---
+++ tests/test_leaf_context.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the parce Python package.
+#
+# Copyright © 2019-2020 by Wilbert Berendsen <info@wilbertberendsen.nl>
+#
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This module is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+Testing CSS parser with pytest-3.
+"""
+
+import sys
+
+sys.path.insert(0, ".")
+
+from parce import *
+
+
+
+class LeafTest(Language):
+    @lexicon
+    def root(cls):
+        yield "A", Text, cls.leaf, cls.second
+        yield "B", Text, cls.leaf
+
+    @lexicon
+    def leaf(cls):
+        yield "B", Text
+        yield "C", Text, -1
+
+    @lexicon
+    def second(cls):
+        yield "C", Text, -1
+
+
+def test_main():
+    d = Document(LeafTest.root, "ACBCBBBB")
+    # removing the first two causes a leaf context to be merged with a non-leaf context
+    # proving that the leaf context stuff is a failed virtue ;-( :-D
+    del d[0:2]
+
+
+if __name__ == "__main__":
+    test_main()
