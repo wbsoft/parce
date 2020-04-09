14420a1dc3efae20c53fff48763e4ad521ea8430
Wilbert Berendsen <wbsoft@xs4all.nl>
2020-04-09 08:56:43 +0200

    make the registry a class, so users can create their own

--- parce/registry.py
+++ parce/registry.py
@@ -22,14 +22,16 @@ Registry of language definitions.
 
 You can use the :func:`suggest` function in this module to find a language
 definition for a particular file type. There is basic functionality to pick a
-language definition based on file name, mime type and the contents of the file.
+language definition based on file name, mime type and/or the contents of the
+file.
 
-Using the :func:`register` function it is also possible to register your own
+Using the :func:`register` function it is possible to register your own
 language definitions at runtime and make them available through parce.
-
 As a service, the bundled languages in ``parce.lang`` are automatically
-registered.
+registered in the global registry.
 
+The global Registry is in the ``registry`` module variable.
+You can also build and populate your own :class:`Registry`.
 """
 
 
@@ -43,83 +45,155 @@ import re
 Item = collections.namedtuple("Item", "name desc filenames mimetypes guesses")
 
 
-_registry = {}
+class Registry:
+    """Registry of language definitions."""
+    def __init__(self):
+        self._registry = {}
+
+    def copy(self):
+        """Return a copy of the registry."""
+        r = type(self)()
+        r._registry.update(self._registry)
+        return r
+
+    def register(self, lexicon_name, *,
+        name,
+        desc,
+        filenames = [],
+        mimetypes = [],
+        guesses = [],
+    ):
+        """Register or update a Language's root lexicon for a particular filename
+        (patterns), particular mime types or based on contents of the file.
+
+        The arguments:
+
+        ``lexicon_name``
+            The fully qualified name of a root lexicon, e.g.
+            ``"parce.lang.css.Css.root"``. Must contain at least two dots.
+        ``name``
+            A human-readable name for the file type
+        ``desc``
+            A short description
+        ``filenames``
+            A list of tuples (pattern, weight). A pattern is a plain filename or a
+            filename with globbing characters, e.g. ``"Makefile"`` or ``"*.c"``,
+            and the weight is a floating point value indicating the probability
+            that the root lexicon should be chosen for this filename (0..1 range).
+        ``mimetypes``
+            A list of tuples (mimetype, weight). A mimetype is a string like
+            ``"text/css"``, the weight is a floating point value indicating the
+            probability that the root lexicon should be chosen for this filename
+            (0..1 range).
+        ``guesses``
+            A list of tuples (regexp, weight). The first 5000 characters of the
+            contents are matched against the regular expression, and when it
+            matches, the weight is added to the already computed weight for this
+            root lexicon.
+
+        """
+        self._registry[lexicon_name] = Item(name, desc, filenames, mimetypes, guesses)
+
+    def suggest(self, filename=None, mimetype=None, contents=None):
+        """Return a list of registered language definitions, sorted on relevance.
+
+        The filename has the most weight, if two have the same weight, the mimetype
+        is looked at; if still the same, the contents are looked at with some
+        heuristic.
+
+        Every item in the returned list is the fully qualified name of the root
+        lexicon, e.g. ``"parce.lang.css.Css.root"``.
+
+        """
+        results = []
+        for lexicon_name, item in self._registry.items():
+            filename_weight = 0
+            if filename:
+                for pattern, weight in item.filenames:
+                    if fnmatch.fnmatch(filename, pattern):
+                        filename_weight += weight
+            mimetype_weight = 0
+            if mimetype:
+                for mtype, weight in item.mimetypes:
+                    if mtype == mimetype:
+                        mimetype_weight = max(mimetype_weight, weight)
+            total_weight = filename_weight + mimetype_weight
+            if total_weight == 0 and contents:
+                for regex, weight in item.guesses:
+                    if re.search(regex, contents[:5000]):
+                        total_weight += weight
+            if total_weight:
+                results.append((lexicon_name, total_weight))
+        results.sort(key=operator.itemgetter(1), reverse=True)
+        return [lexicon_name for lexicon_name, weight in results]
+
+    def registered_item(self, lexicon_name):
+        """Return the stored Item tuple for the specified ``lexicon_name``.
+
+        The Item tuple has the same attributes as the arguments of the
+        :func:`register` function.
+
+        Returns None if the language definition is not available.
+
+        """
+        return self._registry.get(lexicon_name, None)
+
+    def find(self, name):
+        """Find a fully qualified lexicon name for the specified name.
+
+        First, tries to find the exact match on the ``name`` attribute, then a
+        case insensitive match, and then the same for the Language class name.
+
+        """
+        possible = []
+        for lexicon_name, item in self._registry.items():
+            if name == item.name:
+                return lexicon_name
+            if name.lower() == item.name.lower():
+                possible.append(lexicon_name)
+            cls = lexicon_name.rsplit(".", 2)[1]
+            if name.lower() == cls.lower():
+                possible.append(lexicon_name)
+        for lexicon_name in possible:
+            return lexicon_name
+
+    def rename(self, lexicon_name, new_name=None):
+        """Rename the registration of an exiting lexicon.
+
+        This can be useful if you want to override a bundled language definition
+        and use your version.
+
+        If you don't specify a new name, the existing registration is removed.
+
+        """
+        try:
+            old = self._registry.pop(lexicon_name)
+            if new_name:
+                _registry[new_name] = old
+        except KeyError:
+            pass
 
 
-def register(lexicon_name, *,
-    name,
-    desc,
-    filenames = [],
-    mimetypes = [],
-    guesses = [],
-):
-    """Register or update a Language's root lexicon for a particular filename
-    (patterns), particular mime types or based on contents of the file.
-
-    The arguments:
-
-    ``lexicon_name``
-        The fully qualified name of a root lexicon, e.g.
-        ``"parce.lang.css.Css.root"``. Must contain at least two dots.
-    ``name``
-        A human-readable name for the file type
-    ``desc``
-        A short description
-    ``filenames``
-        A list of tuples (pattern, weight). A pattern is a plain filename or a
-        filename with globbing characters, e.g. ``"Makefile"`` or ``"*.c"``,
-        and the weight is a floating point value indicating the probability
-        that the root lexicon should be chosen for this filename (0..1 range).
-    ``mimetypes``
-        A list of tuples (mimetype, weight). A mimetype is a string like
-        ``"text/css"``, the weight is a floating point value indicating the
-        probability that the root lexicon should be chosen for this filename
-        (0..1 range).
-    ``guesses``
-        A list of tuples (regexp, weight). The first 5000 characters of the
-        contents are matched against the regular expression, and when it
-        matches, the weight is added to the already computed weight for this
-        root lexicon.
-
-    """
-    _registry[lexicon_name] = Item(name, desc, filenames, mimetypes, guesses)
+#: the global Registry is in the ``registry`` module variable
+registry = Registry()
 
 
-def suggest(filename=None, mimetype=None, contents=None):
-    """Return a list of registered language definitions, sorted on relevance.
-
-    The filename has the most weight, if two have the same weight, the mimetype
-    is looked at; if still the same, the contents are looked at with some
-    heuristic.
-
-    Every item in the returned list is the fully qualified name of the root
-    lexicon, e.g. ``"parce.lang.css.Css.root"``.
-
-    """
-    results = []
-    for lexicon_name, item in _registry.items():
-        filename_weight = 0
-        if filename:
-            for pattern, weight in item.filenames:
-                if fnmatch.fnmatch(filename, pattern):
-                    filename_weight += weight
-        mimetype_weight = 0
-        if mimetype:
-            for mtype, weight in item.mimetypes:
-                if mtype == mimetype:
-                    mimetype_weight = max(mimetype_weight, weight)
-        total_weight = filename_weight + mimetype_weight
-        if total_weight == 0 and contents:
-            for regex, weight in item.guesses:
-                if re.search(regex, contents[:5000]):
-                    total_weight += weight
-        if total_weight:
-            results.append((lexicon_name, total_weight))
-    results.sort(key=operator.itemgetter(1), reverse=True)
-    return [lexicon_name for lexicon_name, weight in results]
+def register(lexicon_name, **kwargs):
+    """:meth:`~Registry.register` a lexicon in the global registry."""
+    registry.register(lexicon_name, **kwargs)
 
 
-def root_lexicon(lexicon_name):
+def suggest(filename=None, mimetypes=None, contents=None):
+    """:meth:`~Registry.suggest` a lexicon from the global registry."""
+    return registry.suggest(filename, mimetypes, contents)
+
+
+def find(name):
+    """:meth:`~Registry.find` a lexicon by name from the global registry."""
+    return registry.find(name)
+
+
+def root_lexicon(self, lexicon_name):
     """Import the module and return the root lexicon.
 
     Eg, for the ``lexicon_name`` ``"parce.lang.css.Css.root"`` imports the
@@ -130,54 +204,6 @@ def root_lexicon(lexicon_name):
     mod = importlib.import_module(module)
     return getattr(getattr(mod, cls), root)
 
-
-def registered_item(lexicon_name):
-    """Return the stored Item tuple for the specified ``lexicon_name``.
-
-    The Item tuple has the same attributes as the arguments of the
-    :func:`register` function.
-
-    Returns None if the language definition is not available.
-
-    """
-    return _registry.get(lexicon_name, None)
-
-
-def find(name):
-    """Find a fully qualified lexicon name for the specified name.
-
-    First, tries to find the exact match on the ``name`` attribute, then a
-    case insensitive match, and then the same for the Language class name.
-
-    """
-    possible = []
-    for lexicon_name, item in _registry.items():
-        if name == item.name:
-            return lexicon_name
-        if name.lower() == item.name.lower():
-            possible.append(lexicon_name)
-        cls = lexicon_name.rsplit(".", 2)[1]
-        if name.lower() == cls.lower():
-            possible.append(lexicon_name)
-    for lexicon_name in possible:
-        return lexicon_name
-
-
-def rename(lexicon_name, new_name=None):
-    """Rename the registration of an exiting lexicon.
-
-    This can be useful if you want to override a bundled language definition
-    and use your version.
-
-    If you don't specify a new name, the existing registration is removed.
-
-    """
-    try:
-        old = _registry.pop(lexicon_name)
-        if new_name:
-            _registry[new_name] = old
-    except KeyError:
-        pass
 
 
 
