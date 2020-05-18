55d24ca647602a3ac418da2d097ed22e76bac2d3
Wilbert Berendsen <wbsoft@xs4all.nl>
2020-05-18 18:12:34 +0200

    ver bump

--- parce/pkginfo.py
+++ parce/pkginfo.py
@@ -35,7 +35,7 @@ Version = collections.namedtuple("Version", "major minor patch")
 name = "parce"
 
 #: the current version
-version = Version(0, 12, 0)
+version = Version(0, 13, 0)
 version_suffix = ""
 version_string = "{}.{}.{}".format(*version) + version_suffix
 
