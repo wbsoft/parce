08bda1d17db5db82c6a02785d24e3931abdf5997
Wilbert Berendsen <wbsoft@xs4all.nl>
2021-12-01 17:47:36 +0100

    ver bump and chlog update

--- parce/pkginfo.py
+++ parce/pkginfo.py
@@ -35,7 +35,7 @@ Version = collections.namedtuple("Version", "major minor patch")
 name = "parce"
 
 #: the current version
-version = Version(0, 26, 0)
+version = Version(0, 27, 0)
 version_suffix = ""
 version_string = "{}.{}.{}".format(*version) + version_suffix
 
