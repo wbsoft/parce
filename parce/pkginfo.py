bdcd3ffe1d0348cf548d4b0213650377dcf9b65d
Wilbert Berendsen <wbsoft@xs4all.nl>
2023-05-28 14:44:07 +0200

    upd chlog and bump version to 0.33.0

--- parce/pkginfo.py
+++ parce/pkginfo.py
@@ -35,7 +35,7 @@ Version = collections.namedtuple("Version", "major minor patch")
 name = "parce"
 
 #: the current version
-version = Version(0, 32, 0)
+version = Version(0, 33, 0)
 version_suffix = ""
 #: the current version as a string
 version_string = "{}.{}.{}".format(*version) + version_suffix
